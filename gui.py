fda0a1667957b2b2e7e015603e346a343c2c541f
Kelvin <Kelzjr56@gmail.com>
2023-05-17 15:31:10 -0400

    Connected Front-end to back-end, added final touches

--- gui.py
+++ gui.py
@@ -1,21 +1,21 @@
 import PySimpleGUI as gui
+from zip_extractor import extract_archive
 
 gui.theme("Black")
 
 # Choose archive file elements
 label_select_archive = gui.Text("Select Archive")
-input_select_archive = gui.Input()
+input_select_archive = gui.Input(key="archive_input")
 choose_archive_file = gui.FileBrowse("Choose", key='archive')
 
 # Choose destination folder elements
 label_select_destination = gui.Text("Select Destination")
-input_select_destination = gui.Input()
+input_select_destination = gui.Input(key="folder_input")
 choose_destination = gui.FolderBrowse("Choose", key='folder')
 
 # Extract elements
 extract_button = gui.Button("Extract")
 label_output = gui.Text(key='output', text_color="green")
-
 
 # Layout
 layout = [
@@ -26,5 +26,19 @@ layout = [
 
 window = gui.Window("Zip File Extractor", layout=layout)
 
-window.read()
+# Event loop
+while True:
+    event, values = window.read()
+    if event == gui.WIN_CLOSED:
+        break
+    else:
+        print(event, values)
+        archive_path = values['archive']
+        destination_dir = values['folder']
+        extract_archive(archive_path, destination_dir)
+        window['output'].Update(value="Extraction completed!")
+        window['archive_input'].Update(value="")
+        window['folder_input'].Update(value="")
+
+
 window.close()
